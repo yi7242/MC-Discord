e6bee6751d3269063ee0f8decb2199d6cf047b85
yi7242 <47847960+yi7242@users.noreply.github.com>
2023-08-03 08:34:04 +0900

    色々変更

--- bot.py
+++ bot.py
@@ -11,32 +11,33 @@ from sys import exit
 import config
 
 ON_POSIX = 'posix' in sys.builtin_module_names
-log_queue, output_thread = None, None
-
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+server_online = False
+proc = None
 
 def enqueue_output(out, queue):
     for line in iter(out.readline, b''):
         if line == "":
             continue
         else:
-            print(line)
+            print(line, end="")
             queue.put(line)
     out.close()
 
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
-server_online = False
-proc = None
-log_channel = None
+
+# log_channel = None
+# chat_channel = None
 
 
 @client.event
 async def on_ready():
-    global log_channel
+    global log_channel, chat_channel
     print(f'Logged in as: {client.user.name}')
     print(f'With ID: {client.user.id}')
     log_channel = client.get_channel(config.LOG_CHANNEL_ID)
+    chat_channel = client.get_channel(config.CHAT_CHANNEL_ID)
     online_check.start()
     log_output.start()
     await tree.sync(guild=discord.Object(id=config.SERVER_ID))
@@ -67,7 +68,6 @@ async def ipaddress(interaction: discord.Interaction):
 
 @tree.command(guild=discord.Object(id=config.SERVER_ID), description="サーバーを起動します")
 async def start(interaction: discord.Interaction):
-    print("hello")
     global server_online, proc, log_queue, output_thread
     if server_online == False:
         proc = subprocess.Popen(config.BOOT_COMMAND,  stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding="utf-8", bufsize=1, close_fds=ON_POSIX)
@@ -76,16 +76,13 @@ async def start(interaction: discord.Interaction):
         output_thread = Thread(target=enqueue_output, args=(proc.stdout, log_queue))
         output_thread.daemon = True
         output_thread.start()
-        print("open")
         await interaction.response.send_message("起動命令を送信しました")
     else:
         await interaction.response.send_message("サーバーは既にオンラインです")
-        print("nono")
 
 
 @tree.command(guild=discord.Object(id=config.SERVER_ID), description="サーバーを停止します")
 async def stop(interaction: discord.Interaction):
-    print("stop")
     global server_online, proc
     if server_online:
         proc.stdin.write("stop\n")
@@ -104,7 +101,6 @@ async def say(interaction: discord.Interaction, message: str):
         proc.stdin.flush()
         await interaction.response.send_message("発言を送信しました")
     else:
-        type(message)
         print(message.encode('shift_jis'))
         await interaction.response.send_message("サーバーはオフラインです")
 
@@ -113,7 +109,6 @@ async def say(interaction: discord.Interaction, message: str):
 @app_commands.default_permissions(administrator=True)
 async def exe(interaction: discord.Interaction, message: str):
     global server_online, proc
-    print("command is", message)
     if server_online:
         proc.stdin.write(message + "\n")
         proc.stdin.flush()
@@ -171,16 +166,25 @@ async def log_output():
     global server_online, proc
     if server_online:
         log = ""
+        chat_log = ""
         while True:
             try:
-                log += log_queue.get_nowait()
+                log_line = log_queue.get_nowait()
+                if "[minecraft/DedicatedServer]" in log_line:
+                    chat_log += log_line
+                log += log_line
             except Empty:
                 break
-        if log != "":
-            if len(log) > 2000:
-                await log_channel.send(log[:1900] + "... 文字数が2000文字を超えるため省略")
-            else:
-                await log_channel.send(log)
+        await check_send(log, log_channel)
+        await check_send(chat_log, chat_channel)
+
+# 文字数をチェックし、送信する
+async def check_send(log, channel):
+    if log != "":
+        if len(log) > 2000:
+            await channel.send(log[:1900] + "... 文字数が2000文字を超えるため省略")
+        else:
+            await channel.send(log)
 
 async def online_check_loop():
     await online_check.start()
@@ -188,5 +192,3 @@ async def log_output_loop():
     await log_output.start()
 
 client.run(config.APIKEY)
-
-
